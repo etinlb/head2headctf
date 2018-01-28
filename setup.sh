8f90f0ce7afa209126a3c6cc9182f452f37c9cc4
Cate <cate@skysafe.io>
2018-01-28 15:49:20 -0800

    fix nginx config file

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx
 pip3 install -r configs/requirements.txt
 
 cp /etc/nginx/sites-enabled/default nginx.default.bak
-mv configs/nginx.conf /etc/nginx/sites-enabled/default
+cp configs/nginx.conf /etc/nginx/sites-enabled/default
 
 # Need to run this not as root
 # sqlite3 data.db < schema.sql
